c6c342c21b868ff68802691bc8c67f0272b8eaf6
the2nake <home.vn2007@gmail.com>
2024-05-29 16:06:36 +0800

    housekeeping: chassis class clarification

--- include/subzerolib/api/chassis/chassis.hpp
+++ include/subzerolib/api/chassis/chassis.hpp
@@ -2,6 +2,11 @@
 
 class Chassis {
 public:
+  /// @brief move the robot in relative reference frame
+  ///
+  /// @param x lateral component
+  /// @param y vertical component
+  /// @param r rotational component
   virtual void move(double x, double y, double r) = 0;
 
 protected:

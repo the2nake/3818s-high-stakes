c7d75cb8a549540201bdd2c7842c27e74eb95d0c
the2nake <home.vn2007@gmail.com>
2024-06-19 04:02:53 +0700

    housekeeping: small edit to ChassisController docs

--- include/subzerolib/api/control/chassis-controller.hpp
+++ include/subzerolib/api/control/chassis-controller.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include "subzerolib/api/geometry/pose.hpp"
 
-/// @brief f
+/// @brief class that provides movement algorithms with position-based movement
+/// functions
+///
+/// implementations of apporach_pose need not work as a move to pose function on
+/// their own; they simply must approach correctly given that the pose is
+/// roughly aligned with the direction of the drive
 class ChassisController {
 public:
   virtual void approach_pose(pose_s pose, double linv = std::nan("")) = 0;

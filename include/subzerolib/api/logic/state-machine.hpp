4adea8cf8ebba418421d32ec461b9d257b025512
the2nake <home.vn2007@gmail.com>
2024-06-19 13:28:19 +0700

    refactor: use scoped enum for StateMachine

--- include/subzerolib/api/logic/state-machine.hpp
+++ include/subzerolib/api/logic/state-machine.hpp
@@ -1,50 +1,55 @@
 #pragma once
 #include <functional>
 #include <map>
-#include <string>
 
 class StateMachine {
 public:
-  struct State {
-    State(std::string iname = "", std::function<void()> ibehaviour = nullptr,
-          std::map<std::string, std::function<bool()>> iexit_map = {})
-        : name(iname), behaviour(ibehaviour), exit_map(iexit_map) {}
-
-    std::string name;
-    std::function<void()> behaviour;
-    std::map<std::string, std::function<bool()>> exit_map;
+  enum class state_e {
+    null,
   };
 
-  State get_curr_state() { return curr_state; }
+  struct state_data_s {
+    state_data_s(state_e icurr = state_e::null,
+                 std::function<void()> ibehaviour = nullptr,
+                 std::map<state_e, std::function<bool()>> iexit_map = {})
+        : state(icurr), behaviour(ibehaviour), exit_map(iexit_map) {}
+
+    state_e state;
+    std::function<void()> behaviour;
+    std::map<state_e, std::function<bool()>> exit_map;
+  };
+
+  state_data_s get_curr_state_data() { return curr_state_data; }
   void exec_behaviour() {
     bool exit = false;
     do {
-      exit = false;
-      for (auto &exit_pair : curr_state.exit_map) {
+      exit = true;
+      for (auto &exit_pair : curr_state_data.exit_map) {
         if (exit_pair.second()) {
-          curr_state = name_state_lookup.at(exit_pair.first);
+          curr_state_data = name_state_lookup.at(exit_pair.first);
+          exit = false;
         }
       }
     } while (exit);
 
-    curr_state.behaviour();
+    curr_state_data.behaviour();
   }
 
 private:
   StateMachine() {}
 
-  State curr_state;
-  std::map<std::string, State> name_state_lookup;
+  state_data_s curr_state_data;
+  std::map<state_e, state_data_s> name_state_lookup;
 
 public:
   class StateMachineBuilder {
   public:
-    StateMachineBuilder &with_init(std::string state_name);
-    StateMachineBuilder &with_state(State);
+    StateMachineBuilder &with_init(state_e state_name);
+    StateMachineBuilder &with_state(state_data_s);
     StateMachine *build();
 
   private:
-    std::string init_state = "";
-    std::map<std::string, State> blookup;
+    state_e init_state = state_e::null;
+    std::map<state_e, state_data_s> blookup;
   };
 };
--- src/subzerolib/api/logic/state-machine.cpp
+++ src/subzerolib/api/logic/state-machine.cpp
@@ -1,16 +1,16 @@
 #include "subzerolib/api/logic/state-machine.hpp"
 
 StateMachine::StateMachineBuilder &
-StateMachine::StateMachineBuilder::with_init(std::string state_name) {
+StateMachine::StateMachineBuilder::with_init(state_e state_name) {
   init_state = state_name;
 
   return *this;
 }
 
 StateMachine::StateMachineBuilder &
-StateMachine::StateMachineBuilder::with_state(StateMachine::State state) {
-  if (blookup.find(state.name) == blookup.end()) {
-    blookup.emplace(state.name, state);
+StateMachine::StateMachineBuilder::with_state(StateMachine::state_data_s state_data) {
+  if (blookup.find(state_data.state) == blookup.end()) {
+    blookup.emplace(state_data.state, state_data);
   }
 
   return *this;
@@ -25,6 +25,10 @@ StateMachine *StateMachine::StateMachineBuilder::build() {
 
   // validate all states
   for (auto pair : blookup) {
+    if (pair.first != pair.second.state) {
+      return nullptr;
+    }
+
     for (auto exit_pair : pair.second.exit_map) {
       if (blookup.find(exit_pair.first) == blookup.end()) {
         return nullptr;
@@ -32,7 +36,7 @@ StateMachine *StateMachine::StateMachineBuilder::build() {
     }
   }
 
-  obj->curr_state = blookup.at(init_state);
+  obj->curr_state_data = blookup.at(init_state);
   obj->name_state_lookup = blookup;
 
   return obj;

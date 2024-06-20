4dbab9132127ee4c061894b3fc73c295dc9084a1
the2nake <home.vn2007@gmail.com>
2024-06-21 04:08:51 +0700

    feat: notif system for odometry pausing/kill

--- include/subzerolib/api/odometry/odometry.hpp
+++ include/subzerolib/api/odometry/odometry.hpp
@@ -3,6 +3,7 @@
 #include "pros/rtos.hpp"
 #include "subzerolib/api/geometry/point.hpp"
 #include "subzerolib/api/geometry/pose.hpp"
+#include <atomic>
 #include <memory>
 
 class Odometry {
@@ -29,9 +30,34 @@ protected:
 };
 
 struct odom_update_conf_s {
+  ~odom_update_conf_s() { delete signal; }
+  // signals:
+  // 0 = running
+  // 1 = paused
+  // -1 = stopped
+  std::atomic<int> *signal;
   std::shared_ptr<Odometry> odom;
   int delay;
 };
 
+struct task_updater_conf_s {
+public:
+  task_updater_conf_s(pros::Task *itask = nullptr,
+                      std::atomic<int> *isignal = nullptr)
+      : task(itask), signal(isignal) {}
+  pros::Task *task;
+
+  void notify(int new_signal = 0) {
+    *signal = new_signal;
+    task->notify();
+  }
+
+  int get_signal() { return signal->load(); }
+
+private:
+  std::atomic<int> *signal;
+};
+
 void update_odometry_callback_loop(void *params);
-pros::Task *automatic_update(std::shared_ptr<Odometry> odom, int ms_delay);
+task_updater_conf_s automatic_update(std::shared_ptr<Odometry> odom,
+                                     int ms_delay);
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "pros/abstract_motor.hpp"
+#include "pros/misc.h"
 #include "pros/misc.hpp"
 #include "subzerolib/api.hpp"
 #include "subzerolib/api/odometry/imu_odometry.hpp"
@@ -24,13 +25,14 @@ std::unique_ptr<pros::AbstractMotor>
     bl(new pros::Motor(11, pros::v5::MotorGears::green,
                        pros::v5::MotorUnits::deg));
 std::shared_ptr<XChassis> chassis = nullptr;
+std::shared_ptr<AbstractGyro> imu(new AbstractImuGyro(8));
+
 // TODO: configure ports
 std::shared_ptr<AbstractEncoder> odom_x(new AbstractRotationEncoder(9, false));
 std::shared_ptr<AbstractEncoder> odom_y(new AbstractRotationEncoder(10, false));
 std::shared_ptr<Odometry> odom = nullptr;
-std::shared_ptr<AbstractGyro> imu(new AbstractImuGyro(8));
+task_updater_conf_s odom_updater;
 
-pros::Task *odom_task;
 void initialize() {
   chassis = XChassis::XChassisBuilder()
                 .with_motors(std::move(fl), std::move(fr), std::move(br),
@@ -42,7 +44,7 @@ void initialize() {
              .with_x_enc(odom_x, Odometry::encoder_conf_s(0, 0))
              .with_y_enc(odom_y, Odometry::encoder_conf_s(0, 0))
              .build();
-  odom_task = automatic_update(odom, 10);
+  odom_updater = automatic_update(odom, 10);
 }
 
 void disabled() {}
@@ -65,6 +67,20 @@ void opcontrol() {
     chassis->move(x * cos(rad) + y * sin(rad), y * cos(rad) - x * sin(rad),
                   0.75 * r);
     // chassis->move(x, y, r);
+
+    if (master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+      // test pausing/starting odometry
+      if (odom_updater.get_signal() == 1) {
+        odom_updater.notify(0);
+      } else {
+        odom_updater.notify(1);
+      }
+    }
+    // print odometry output
+    auto pose = odom->get_pose();
+    pros::screen::print(pros::E_TEXT_MEDIUM, 0, "x: %f, y: %f, h: %f", pose.x,
+                        pose.y, pose.heading());
+
     pros::delay(20); // Run for 20 ms then update
   }
 
@@ -74,6 +90,6 @@ void opcontrol() {
   //   no need for imu, it's a smart pointer
   //   no need in ImuOdometry, everything is a smart pointer
   // notify to delete the odometry update task
-  odom_task->notify();
+  odom_updater.notify(-1);
   // delete pointers
 }

30eaea62576b0fffa16db0419b16a94fa723a40a
the2nake <home.vn2007@gmail.com>
2024-07-30 16:17:09 +0800

    cleanup: test mp: removed some comments and changed naming

--- test-src/test-mp.cpp
+++ test-src/test-mp.cpp
@@ -52,49 +52,48 @@ int main() {
   generator->set_resolution(0.01);
 
   // generate the curve using a catmull rom spline
-  std::vector<pose_s> ctrl = {
+  std::vector<pose_s> ctrl_points = {
       pose_s{  0.0,  0.0,   0.0},
       pose_s{  0.4,  0.6,  45.0},
       pose_s{ -0.2,  0.6,  60.0},
       pose_s{-0.75, 0.75, -45.0}
   };
-  CatmullRomSpline spline(ctrl);
+  CatmullRomSpline spline(ctrl_points);
   spline.pad_velocity({0.5, 0.5}, {-0.25, 0.25});
-  std::vector<spline_point_s> sampled = spline.sample_kinematics(300);
-  std::vector<trajectory_point_s> profile(sampled.size());
-  for (int i = 0; i < sampled.size(); ++i) {
-    profile[i] = sampled[i];
+  std::vector<spline_point_s> sampled_points = spline.sample_kinematics(300);
+  std::vector<trajectory_point_s> generated_profile(sampled_points.size());
+  for (int i = 0; i < sampled_points.size(); ++i) {
+    generated_profile[i] = sampled_points[i];
   }
   std::vector<int> ctrl_indices;
-  for (auto &c : ctrl) {
-    int i = find_pose_index(sampled, c);
-    profile[i].x = c.x;
-    profile[i].y = c.y;
-    profile[i].h = c.h;
+  for (auto &c : ctrl_points) {
+    int i = find_pose_index(sampled_points, c);
+    generated_profile[i].x = c.x;
+    generated_profile[i].y = c.y;
+    generated_profile[i].h = c.h;
     ctrl_indices.push_back(i);
   }
 
   // apply profile to path
-  generator->generate(profile.back().s);
+  generator->generate(generated_profile.back().s);
   // dynamic_cast<TrapezoidalMotionProfile *>(generator)->print();
-  for (int i = 0; i < profile.size(); ++i) {
+  for (int i = 0; i < generated_profile.size(); ++i) {
     LinearMotionProfile::profile_point_s motion =
-        generator->get_point_at_distance(profile[i].s);
-    profile[i].t = motion.t;
-    if (i < profile.size() - 1) {
-      double dx = profile[i + 1].x - profile[i].x;
-      double dy = profile[i + 1].y - profile[i].y;
+        generator->get_point_at_distance(generated_profile[i].s);
+    generated_profile[i].t = motion.t;
+    if (i < generated_profile.size() - 1) {
+      double dx = generated_profile[i + 1].x - generated_profile[i].x;
+      double dy = generated_profile[i + 1].y - generated_profile[i].y;
       double ds = hypot(dx, dy); // don't use s here
-      profile[i].vx = motion.v / ds * dx;
-      profile[i].vy = motion.v / ds * dy;
+      generated_profile[i].vx = motion.v / ds * dx;
+      generated_profile[i].vy = motion.v / ds * dy;
     }
   }
-  // the endpoint is assumed to be a control point
-  profile.back().vx = 0;
-  profile.back().vy = 0;
+  generated_profile.back().vx = 0;
+  generated_profile.back().vy = 0;
 
   // lerp headings and angular velocity
-  for (int i = 0; i < profile.size(); ++i) {
+  for (int i = 0; i < generated_profile.size(); ++i) {
     if (std::find(ctrl_indices.begin(), ctrl_indices.end(), i) ==
         ctrl_indices.end()) {
       int start_index = 0;
@@ -106,41 +105,27 @@ int main() {
           break;
         }
       }
-      double long_s = profile[end_index].s - profile[start_index].s;
-      double curr_s = profile[i].s - profile[start_index].s;
-      double factor = curr_s / long_s;
-      double begin_h = profile[start_index].h;
-      double end_h = profile[end_index].h;
-      profile[i].h = begin_h + factor * shorter_turn(begin_h, end_h, 360.0);
-      /*
-      auto &p = profile[i];
-      printf("i=%d s=%.3f (%.2f, %.2f) h=%.0f factor=%.3f i0=%d i1=%d\n",
-             i,
-             p.s,
-             p.x,
-             p.y,
-             p.h,
-             factor,
-             start_index,
-             end_index);
-             */
-    } else {
-      /*
-      auto &p = profile[i];
-      printf("i=%d s=%.3f (%.2f, %.2f) h=%.0f\n", i, p.s, p.x, p.y, p.h);
-      */
+      double factor =
+          (generated_profile[i].s - generated_profile[start_index].s) /
+          (generated_profile[end_index].s - generated_profile[start_index].s);
+      double h0 = generated_profile[start_index].h;
+      double h1 = generated_profile[end_index].h;
+      generated_profile[i].h = h0 + factor * shorter_turn(h0, h1, 360.0);
     }
   }
-  profile.back().vh = 0;
+  generated_profile.back().vh = 0;
 
   // generate vh
-  for (int i = 0; i < profile.size() - 1; ++i) {
-    double dt = profile[i + 1].t - profile[i].t;
-    // printf("%f\n", dt);
-    profile[i].vh = shorter_turn(profile[i].h, profile[i + 1].h, 360.0) / dt;
+  for (int i = 0; i < generated_profile.size() - 1; ++i) {
+    double dt = generated_profile[i + 1].t - generated_profile[i].t;
+    generated_profile[i].vh = shorter_turn(generated_profile[i].h,
+                                           generated_profile[i + 1].h,
+                                           360.0) /
+                              dt;
   }
 
-  for (trajectory_point_s &p : profile) {
+  // show the profile
+  for (trajectory_point_s &p : generated_profile) {
     printf("t=%.3f s=%.3f (%.2f, %.2f) h=%.3f vh=%.1f vx=%.2f vy=%.2f v=%.2f\n",
            p.t,
            p.s,

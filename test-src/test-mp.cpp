1cd1e643906d768080d079712fcb78c3427327d2
the2nake <home.vn2007@gmail.com>
2024-08-07 21:02:18 +0800

    refactor: test mp: added comments, changed naming

--- test-src/test-mp.cpp
+++ test-src/test-mp.cpp
@@ -90,9 +90,9 @@ int find_pose_index(std::vector<spline_point_s> &vec, pose_s pose) {
 
 int main() {
   // set up a linear motion profile
-  StarChassisKinematics kinematics(1.73, 0.35, 0.37);
-  LinearMotionProfile *generator = new TrapezoidalMotionProfile{1.73, 4, 4};
-  generator->set_resolution(0.01);
+  StarChassisKinematics kinematics{1.73, 0.35, 0.37};
+  auto lin_profile = new TrapezoidalMotionProfile{1.73, 4, 4};
+  lin_profile->set_resolution(0.01);
 
   // generate the curve using a catmull rom spline
   std::vector<pose_s> ctrl_points = {
@@ -110,26 +110,32 @@ int main() {
     trajectory[i] = sampled_points[i];
   }
   std::vector<int> ctrl_indices;
-  for (auto &c : ctrl_points) {
-    int i = find_pose_index(sampled_points, c);
-    trajectory[i].x = c.x;
-    trajectory[i].y = c.y;
-    trajectory[i].h = c.h;
+
+  for (auto &ctrl_point : ctrl_points) {
+    int i = find_pose_index(sampled_points, ctrl_point);
+    auto &traj_ctrl = trajectory[i];
+    traj_ctrl.x = ctrl_point.x;
+    traj_ctrl.y = ctrl_point.y;
+    traj_ctrl.h = ctrl_point.h;
     ctrl_indices.push_back(i);
   }
 
   // apply profile to path
-  generator->generate(trajectory.back().s);
+  lin_profile->generate(trajectory.back().s);
   for (int i = 0; i < trajectory.size(); ++i) {
-    LinearMotionProfile::profile_point_s motion =
-        generator->get_point_at_distance(trajectory[i].s);
-    trajectory[i].t = motion.t;
+    auto lin_point = lin_profile->get_point_at_distance(trajectory[i].s);
+    trajectory[i].t = lin_point.t;
+
     if (i < trajectory.size() - 1) {
-      double dx = trajectory[i + 1].x - trajectory[i].x;
-      double dy = trajectory[i + 1].y - trajectory[i].y;
+      auto &curr = trajectory[i];
+      auto &next = trajectory[i + 1];
+
+      double dx = next.x - curr.x;
+      double dy = next.y - curr.y;
       double ds = hypot(dx, dy); // don't use s here
-      trajectory[i].vx = motion.v / ds * dx;
-      trajectory[i].vy = motion.v / ds * dy;
+
+      curr.vx = lin_point.v / ds * dx;
+      curr.vy = lin_point.v / ds * dy;
     }
   }
   trajectory.back().vx = 0;
@@ -137,10 +143,11 @@ int main() {
 
   // lerp headings and angular velocity
   for (int i = 0; i < trajectory.size(); ++i) {
-    if (std::find(ctrl_indices.begin(), ctrl_indices.end(), i) ==
-        ctrl_indices.end()) {
-      int i0 = 0;
-      int i1 = 0;
+    bool not_ctrl_point =
+        std::find(ctrl_indices.begin(), ctrl_indices.end(), i) ==
+        ctrl_indices.end();
+    if (not_ctrl_point) {
+      int i0 = 0, i1 = 0;
       for (int j = 1; j < ctrl_indices.size(); ++j) {
         if (ctrl_indices[j] > i) {
           i0 = ctrl_indices[j - 1];
@@ -148,19 +155,20 @@ int main() {
           break;
         }
       }
-      double factor = (trajectory[i].s - trajectory[i0].s) /
-                      (trajectory[i1].s - trajectory[i0].s);
-      double h0 = trajectory[i0].h;
-      double h1 = trajectory[i1].h;
-      trajectory[i].h = h0 + factor * shorter_turn(h0, h1, 360.0);
+      double s_pct = (trajectory[i].s - trajectory[i0].s) /
+                     (trajectory[i1].s - trajectory[i0].s);
+      double h0 = trajectory[i0].h, h1 = trajectory[i1].h;
+      trajectory[i].h = h0 + s_pct * shorter_turn(h0, h1, 360.0);
     }
   }
 
   // generate vh
   for (int i = 0; i < trajectory.size() - 1; ++i) {
-    double dt = trajectory[i + 1].t - trajectory[i].t;
-    trajectory[i].vh =
-        shorter_turn(trajectory[i].h, trajectory[i + 1].h, 360.0) / dt;
+    auto &curr = trajectory[i];
+    auto &next = trajectory[i + 1];
+
+    double dt = next.t - curr.t;
+    trajectory[i].vh = shorter_turn(next.h, next.h, 360.0) / dt;
   }
   trajectory.back().vh = 0;
 
@@ -172,15 +180,19 @@ int main() {
   dts.front() = 0.0;
   for (int i = 1; i < trajectory.size(); ++i) {
     auto &traj_p = trajectory[i];
+
+    // generate wheel velocities
     point_s local_v = rotate_acw(traj_p.vx, traj_p.vy, traj_p.h);
     auto wheel_vels =
         kinematics.get_wheel_vels(local_v.x, local_v.y, in_rad(traj_p.vh));
 
+    // vel_scale used to accumulate scaling ratios
     double vel_scale = 1.0;
     for (int j = 0; j < wheel_vels.size(); ++j) {
       double mag = std::abs(wheel_vels[j]);
       double max = std::abs(max_vels[j]);
 
+      // scale uniformly if exceeding
       if (mag > max) {
         double scale = max / mag;
         for (auto &v : wheel_vels) {
@@ -189,14 +201,16 @@ int main() {
         vel_scale *= 0.999999 * scale;
       }
     }
-
+    // scale time and store
     double dt = trajectory[i].t - trajectory[i - 1].t;
     dts[i] = dt / vel_scale;
+    // scale velocities to match wheel velocity changes
     traj_p.vx *= vel_scale;
     traj_p.vy *= vel_scale;
     traj_p.vh *= vel_scale;
   }
 
+  // integrate new time deltas
   for (int i = 1; i < trajectory.size(); ++i) {
     trajectory[i].t = trajectory[i - 1].t + dts[i];
   }
